b410c600b90d5a17c82f88f1d6d3991f1bace60d
MFShujath <mmfshujath@gmail.com>
2016-10-05 06:55:52 +0530

    Update Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,10 +1,5 @@
-ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
@@ -12,10 +7,12 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.msm8226
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
+#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
